a6ec303cdd3d111010f483f47d1cbb1ca2d78b5a
Vlad_Nozhkin <mega.nozhkin@mail.ru>
2021-01-12 20:36:33 +0300

    Инициализирован проект HouseVope

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // entry: {
+    //     main: './src/app.js'
+    // },
+    output: {
+        filename: 'script/bundle.js'
+    },
+    //...
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style/[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+    }
+};
